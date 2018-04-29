f3046bc0b58eab84d3bcd73f299fdabd213e97d9
Gteoharov <Gteoharov@users.noreply.github.com>
2018-04-29 13:00:44 +0300

    Creating the basics of the application

--- MiraclePill/ViewController.swift
+++ MiraclePill/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    
+    @IBOutlet weak var agePicker: UIPickerView!
+    
+    @IBOutlet weak var agePickerButton: UIButton!
+    
+    var ages = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        agePicker.dataSource = self
+        agePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func agePickerPressedButton(_ sender: Any) {
+        agePicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return ages.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(ages[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        agePickerButton.setTitle(String("Your Age: \(ages[row])"), for: UIControlState.normal)
+        agePicker.isHidden = true
+    }
 
 }
 
